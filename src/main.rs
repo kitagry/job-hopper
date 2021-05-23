63cf58fc8c644d02b892fe10d9e0f6394981e4e3
Ryo Kitagawa <kitadrum50@gmail.com>
2021-05-23 22:22:37 +0900

    Create api

--- src/main.rs
+++ src/main.rs
@@ -1,77 +1,97 @@
-use futures::{StreamExt, TryStreamExt};
 use k8s_openapi::api::batch::v1::Job;
+use k8s_openapi::api::batch::v1beta1::{CronJob, JobTemplateSpec};
+use k8s_openapi::apimachinery::pkg::apis::meta::v1::ObjectMeta;
 use kube::{
-    api::{Api, DeleteParams, ListParams, PostParams, WatchEvent},
+    api::{Api, ListParams, PostParams},
     Client,
 };
+use rand::distributions::Alphanumeric;
+use rand::{thread_rng, Rng};
 use serde_json::json;
+use std::convert::Infallible;
+use warp::{Filter, Reply};
+
+async fn list_cronjobs(k8s_client: Client) -> Result<impl Reply, Infallible> {
+    let cronjobs: Api<CronJob> = Api::namespaced(k8s_client, "");
+    let lp = ListParams::default().timeout(20);
+
+    match cronjobs.list(&lp).await {
+        Ok(cronjobs) => {
+            let cronjobs: Vec<JobTemplateSpec> = cronjobs
+                .iter()
+                .map(|c| {
+                    let mut t = c.spec.clone().unwrap().job_template;
+                    let mut metadata = ObjectMeta::default();
+                    metadata.name = Some(create_tmp_job_name(c.metadata.name.clone()));
+                    metadata.namespace = c.metadata.namespace.clone();
+                    t.metadata = Some(metadata);
+                    t
+                })
+                .collect();
+            Ok(warp::reply::with_status(
+                warp::reply::json(&cronjobs),
+                http::StatusCode::OK,
+            ))
+        }
+        Err(_) => Ok(warp::reply::with_status(
+            warp::reply::json(&json!({"message": "failed"})),
+            http::StatusCode::INTERNAL_SERVER_ERROR,
+        )),
+    }
+}
+
+fn create_tmp_job_name(s: Option<String>) -> String {
+    let rand_string: String = thread_rng()
+        .sample_iter(&Alphanumeric)
+        .take(6)
+        .map(char::from)
+        .collect();
+    let rand_string = rand_string.to_lowercase();
+    match s {
+        Some(s) => format!("{}-{}", s, rand_string),
+        None => format!("job-hopper-{}", rand_string),
+    }
+}
+
+async fn create_job(k8s_client: Client, job: Job) -> Result<impl Reply, Infallible> {
+    let jobs: Api<Job> = Api::namespaced(k8s_client, &job.metadata.namespace.clone().unwrap());
+    let pp = PostParams::default();
+    match jobs.create(&pp, &job).await {
+        Ok(_) => Ok(warp::reply::with_status(
+            warp::reply::json(&json!({"message": "ok"})),
+            http::StatusCode::CREATED,
+        )),
+        Err(e) => {
+            Ok(warp::reply::with_status(
+            warp::reply::json(&json!({"message": e.to_string()})),
+            http::StatusCode::BAD_REQUEST,
+        ))},
+    }
+}
+
+fn with_k8s(k8s_client: Client) -> impl Filter<Extract = (Client,), Error = Infallible> + Clone {
+    warp::any().map(move || k8s_client.clone())
+}
+
+fn json_body() -> impl Filter<Extract = (Job,), Error = warp::Rejection> + Clone {
+    warp::body::content_length_limit(1024 * 16).and(warp::body::json())
+}
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     let client = Client::try_default().await?;
-    let namespace = std::env::var("NAMESPACE").unwrap_or("default".into());
+    let list_cronjobs = warp::path!("cronjob")
+        .and(with_k8s(client.clone()))
+        .and_then(list_cronjobs);
 
-    let job_name = "empty-job";
-    let my_job = serde_json::from_value(json!({
-        "apiVersion": "batch/v1",
-            "kind": "Job",
-            "metadata": {
-                "name": job_name
-            },
-        "spec": {
-            "template": {
-                "metadata": {
-                    "name": "pod"
-                },
-                "spec":{
-                    "containers": [{
-                        "name": "empty",
-                        "image": "alpine:latest"
-                    }],
-                    "restartPolicy": "Never"
-                }
-            }
-        }
-    }))?;
+    let create_job = warp::post()
+        .and(warp::path("job"))
+        .and(with_k8s(client.clone()))
+        .and(json_body())
+        .and_then(create_job);
 
-    let jobs: Api<Job> = Api::namespaced(client, &namespace);
-    let pp = PostParams::default();
+    let routes = list_cronjobs.or(create_job);
 
-    jobs.create(&pp, &my_job).await?;
-
-    let lp = ListParams::default()
-        .fields(&format!("metadata.name={}", job_name))
-        .timeout(20);
-    let mut stream = jobs.watch(&lp, "").await?.boxed();
-
-    while let Some(status) = stream.try_next().await? {
-        match status {
-            WatchEvent::Added(s) => println!("added {}", s.metadata.name.unwrap_or("".to_string())),
-            WatchEvent::Modified(s) => {
-                let name = s.metadata.name.unwrap_or("".to_string());
-                let current_status = s.status.clone().expect("status is missing");
-                match current_status.completion_time {
-                    Some(_) => {
-                        println!("Modified: {} is complete", name);
-                        break;
-                    }
-                    _ => println!("Modified: {} is running", name),
-                }
-            }
-            WatchEvent::Deleted(s) => {
-                println!("Deleted {}", s.metadata.name.unwrap_or("".to_string()))
-            }
-            WatchEvent::Error(s) => eprintln!("{}", s),
-            _ => {}
-        }
-    }
-
-    println!("delete the job");
-    let mut dp = DeleteParams::default();
-    dp.dry_run = true;
-    jobs.delete(job_name, &dp).await?;
-    dp.dry_run = false;
-    jobs.delete(job_name, &dp).await?;
-
+    warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
     Ok(())
 }
